9e080ebf95f2ecce593e1d6f635569f2c2762c13
Fábio Dantas <fabio.tot.brisa@gmail.com>
2025-09-03 22:36:39 -0300

    Primeiro commit

---
+++ js/app.js
@@ -0,0 +1,29 @@
+valorTotal = 0;
+
+function adicionar(){
+
+//Recuperar valores: nome do produto, preço e quantidade
+let produto = document.getElementById('produto').value;
+let quantidade = document.getElementById('quantidade').value;
+let nomeProduto = produto.split('-')[0];
+let precoProduto = produto.split('R$')[1];
+
+//Calcular o preço do subproduto (multiplicar o preço unitário pela quantidade de itens do mesmo produto)
+let precoSubproduto = quantidade * precoProduto;
+
+//Adicionar itens ao carrinho
+let carrinho = document.getElementById('lista-produtos');
+carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${precoProduto},00</span>
+        </section>`;
+
+//Calcular o preço da compra total
+valorTotal = valorTotal + precoSubproduto;
+let mostrarValorTotal = document.getElementById('valor-total');
+mostrarValorTotal.innerHTML =       `<p class="carrinho__total">
+        Total: <span class="texto-azul" id="valor-total">R$${valorTotal},00</span>
+      </p>`;
+}
+
+function limpar(){
+}
